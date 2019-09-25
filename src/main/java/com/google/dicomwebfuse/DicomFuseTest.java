8287ae7b1dd0a5dd96118e83956b464ab216d92b
Artyom Gordiyenko <artyom.gordienko@quantumsoft.ru>
2019-09-25 12:10:13 +0700

    Update logger configuration

--- src/main/java/com/google/dicomwebfuse/DicomFuseTest.java
+++ src/main/java/com/google/dicomwebfuse/DicomFuseTest.java
@@ -18,25 +18,33 @@ import com.google.dicomwebfuse.mount.DicomFuseConfigurator;
 import com.google.dicomwebfuse.mount.DicomFuseTestMount;
 import com.google.dicomwebfuse.mount.Mount;
 import com.google.dicomwebfuse.parser.FuseTestArguments;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 
 public class DicomFuseTest {
 
   private static final Logger LOGGER = LogManager.getLogger();
 
   public static void main(String[] args) {
-    LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-    Configuration config = ctx.getConfiguration();
-    config.getRootLogger().removeAppender("Console");
-    ctx.updateLoggers();
+    changeConsoleLoggingLevel();
     FuseTestArguments fuseTestArguments = new FuseTestArguments();
     DicomFuseConfigurator.configureDicomFuse(args, fuseTestArguments);
     if (!fuseTestArguments.help) {
       Mount<FuseTestArguments> mount = new DicomFuseTestMount();
       mount.mountDicomFuseFS(fuseTestArguments);
     }
+  }
+
+  private static void changeConsoleLoggingLevel() {
+    LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+    Configuration config = ctx.getConfiguration();
+    LoggerConfig rootLoggerConfig = config.getRootLogger();
+    rootLoggerConfig.removeAppender("Console");
+    rootLoggerConfig.addAppender(config.getAppender("Console"), Level.ERROR, null);
+    ctx.updateLoggers();
   }
 }
